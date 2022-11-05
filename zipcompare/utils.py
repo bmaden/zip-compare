b4eea428115cfb7bc3839b2665d8ceb063ecbfe4
Brett Maden <os@maden.com>
2022-11-05 15:40:17 -0400

    cleanup

--- zipcompare/utils.py
+++ zipcompare/utils.py
@@ -7,11 +7,11 @@ import zipfile
 
 def traverse(zipfile_name: typing.BinaryIO) -> typing.Dict[str, str]:
     data = dict()
-    with zipfile.ZipFile(zipfile_name, "r") as zipdata:
-        for info in zipdata.infolist():
+    with zipfile.ZipFile(zipfile_name, "r") as zip_data:
+        for info in zip_data.infolist():
             if info.is_dir() is False:
                 hasher = hashlib.new("sha256")
-                hasher.update(zipdata.read(info.filename))
+                hasher.update(zip_data.read(info.filename))
                 data[info.filename] = hasher.hexdigest()
 
     return data
@@ -21,10 +21,10 @@ def traverse_archive(zipfile_name: str) -> dict:
     with open(zipfile_name, "rb") as zip_open:
         file_meta = dict(root=traverse(zip_open), nested=dict())
 
-    with zipfile.ZipFile(zipfile_name, "r") as zipdata:
+    with zipfile.ZipFile(zipfile_name, "r") as zip_data:
         for f in file_meta["root"].keys():
             if re.match(r".*\.(jar|war|ear|zip)$", f, re.IGNORECASE):
-                with zipdata.open(f) as internal_archive:
+                with zip_data.open(f, "r") as internal_archive:
                     file_meta["nested"][f] = traverse(internal_archive)
 
     return file_meta
