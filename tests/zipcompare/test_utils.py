b99e0c53780c69bb626a909c50bc2c4f71491e1b
Brett Maden <os@maden.com>
2022-11-05 15:01:33 -0400

    go one level deep; use sha256 instead of crc32

--- tests/zipcompare/test_utils.py
+++ tests/zipcompare/test_utils.py
@@ -1,13 +1,21 @@
-import pathlib
+import pytest
 import zipcompare.utils
 
 
-def test_traversal(resource_path):
+def test_traverse_archive(resource_path):
     zip_path = resource_path / "test.zip"
+    meta = zipcompare.utils.traverse_archive(str(zip_path))
 
-
-    zipcompare.utils.traverse(str(zip_path))
-
-
-# def test_something2():
-#     assert 1 == 2
+    # directory (doesn't get added to output)
+    with pytest.raises(KeyError):
+        assert meta["root"]["dir_b/"] == "dir"
+    # file in root archive
+    assert (
+        meta["root"]["dir_a/x/y/z/deep_file.txt"]
+        == "98253840c84dd789a6140d2474486fd1d79bc11e92d01ff16d3e5cfbbae92b29"
+    )
+    # file in nested archive
+    assert (
+        meta["nested"]["zip/zipped_file.zip"]["dir_y/file_y.txt"]
+        == "34e574e523ae82738ba84d2a83c8270f0384976f219527307da5ec3a4b4cc680"
+    )
--- zipcompare/utils.py
+++ zipcompare/utils.py
@@ -1,24 +1,31 @@
+import hashlib
 import re
 import sys
 import typing
 import zipfile
 
 
-def traverse(zipfile_name: str) -> typing.Dict[str, typing.List[zipfile.ZipInfo]]:
+def traverse(zipfile_name: typing.BinaryIO) -> typing.Dict[str, str]:
+    data = dict()
     with zipfile.ZipFile(zipfile_name, "r") as zipdata:
-        return {info.filename: info.CRC for info in zipdata.infolist()}
+        for info in zipdata.infolist():
+            if info.is_dir() is False:
+                hasher = hashlib.new("sha256")
+                hasher.update(zipdata.read(info.filename))
+                data[info.filename] = hasher.hexdigest()
+
+    return data
 
 
 def traverse_archive(zipfile_name: str) -> dict:
-    file_meta = dict(root=traverse(zipfile_name), nested=dict())
+    with open(zipfile_name, "rb") as zip_open:
+        file_meta = dict(root=traverse(zip_open), nested=dict())
 
-    for f in file_meta["root"].keys():
-        print(f"testing: {f}")
-        if re.match(r".*\.(jar|war|ear|zip)$", f, re.IGNORECASE):
-            print(f"extracting: {f}")
-            z = zipfile.ZipFile(zipfile_name)
-            f_open = z.open(f)
-            file_meta["nested"][f] = traverse(f_open)
+    with zipfile.ZipFile(zipfile_name, "r") as zipdata:
+        for f in file_meta["root"].keys():
+            if re.match(r".*\.(jar|war|ear|zip)$", f, re.IGNORECASE):
+                with zipdata.open(f) as internal_archive:
+                    file_meta["nested"][f] = traverse(internal_archive)
 
     return file_meta
 
